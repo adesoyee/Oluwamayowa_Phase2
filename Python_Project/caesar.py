197172a736960dc80bb7456bd2e049525ee5db16
adesoye@usc.edu <adesoyee@yahoo.com>
2024-01-29 14:02:32 -0800

    add

--- Python_Project/caesar.py
+++ Python_Project/caesar.py
@@ -1,15 +1,33 @@
 import sys
 
-
 def encrypt(message, k):
-    return
-
+    encrypted_message = ""
+    for char in message:
+        if char.isalpha():
+            shifted = ord(char) + k
+            if char.islower():
+                if shifted > ord('z'):
+                    shifted -= 26
+                elif shifted < ord('a'):
+                    shifted += 26
+            elif char.isupper():
+                if shifted > ord('Z'):
+                    shifted -= 26
+                elif shifted < ord('A'):
+                    shifted += 26
+            encrypted_message += chr(shifted)
+        else:
+            encrypted_message += char
+    return encrypted_message
 
 def decrypt(message, k):
-    return
+    return encrypt(message, -k)
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <message> <key>")
+        sys.exit(1)
     # take in first arg as word
     message = sys.argv[1]
     # take in second arg as int key
